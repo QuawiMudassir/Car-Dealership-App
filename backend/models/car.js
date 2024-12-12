fe0ece3200c5bcfb067763e2f558e061ca0c7939
Ghazala <157250706+ghazalaanjum12@users.noreply.github.com>
2024-12-12 07:18:16 -0500

    Commit for getCars, getCarById, getFeaturedCars, getMakes

--- backend/models/car.js
+++ backend/models/car.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const carSchema = new mongoose.Schema({
-  make: { type: String, required: true },
+  make: { type: String, required: true, index: true},
   model: { type: String, required: true },
   year: { type: Number, required: true },
   color: { type: String, required: true },
